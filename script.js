9449903c92d8e1050ac7ac6c426196e1339862c7
Roland Hesse <rolandhesse11783@gmail.com>
2024-05-23 11:50:47 +0200

    implement first basic pop-up-solution

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
 import explanationBoxes from "./data.js";
 const imageContainer = document.querySelector(".image-container");
 
-function createExplanationBox(box) {
+function createCarTags(box) {
     return `
-    <div class="explanation-box" id="${box.id}">
-    <div class="explanation-box__header">
-        <button class="explanation-box__button" >
+    <div class="car-tag-container" id="${box.id}">
+    <div class="car-tag-container__header" id="${box.id}">
+        <button class="car-tag-container__button" >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M11 11V7H13V11H17V13H13V17H11V13H7V11H11ZM12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20Z"></path>
             </svg>
         </button>
-        <h3 class="explanation-box__heading">${box.heading}</h3>
+        <h3 class="car-tag-container__heading">${box.heading}</h3>
     </div>
-    <p class="explanation-box__text" style="display: none">${box.text}</p>
+</div>
+<div class="pop-up-container" style="display: none">
+<h3 class="pop-up-container__heading">${box.heading}</h3>
+<p class="pop-up-container__text">${box.text}</p>
+<button type="button" class="pop-up-container__button">Close</button>
 </div>
 `};
 
 
 explanationBoxes.forEach((box) => {
-imageContainer.innerHTML += createExplanationBox(box);
+imageContainer.innerHTML += createCarTags(box);
 });
 
-function createExplanationBoxEventListeners() {
-  const explanationBoxButtons = document.querySelectorAll(".explanation-box__button");
+function createCarTagEventListeners() {
+  const carTagButtons = document.querySelectorAll(".car-tag-container__button");
   
-    explanationBoxButtons.forEach((button) => {button.addEventListener("click", (event) => {
-        const explanationText = event.currentTarget.parentNode.parentNode.querySelector(".explanation-box__text");
-        explanationText.style.display = explanationText.style.display === "none"  ? "block" : "none";
-        const explanationBox = event.currentTarget.parentNode.parentNode;    
-        explanationBox.style.zIndex = explanationBox.style.zIndex === "" ? "1000" : "";
+    carTagButtons.forEach((button) => {button.addEventListener("click", (event) => {
+      const popUpContainer = event.currentTarget.parentNode.parentNode.nextElementSibling;
+      popUpContainer.style.display = popUpContainer.style.display === "none" ? "block" : "none";
     }
-    )})
+    )});
 }
 
-createExplanationBoxEventListeners();
+function createPopUpEventListener() {
+    const popUpButtons = document.querySelectorAll(".pop-up-container__button");
+
+    popUpButtons.forEach((button) => {button.addEventListener("click", (event) => {
+        const popUpContainer = event.currentTarget.parentNode;
+        popUpContainer.style.display = popUpContainer.style.display === "none" ? "block" : "none";
+    })});
+};
+
+createCarTagEventListeners();
+createPopUpEventListener();
